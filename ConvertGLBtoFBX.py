699bb017dbd82d8b2f0e94ea7e5c48b14091868d
Amirsaleh Naderzadeh Mehrabani <104119787+SalehMehrabani@users.noreply.github.com>
2024-01-01 11:40:57 +0330

    Update ConvertGLBtoFBX.py

--- ConvertGLBtoFBX.py
+++ ConvertGLBtoFBX.py
@@ -1,21 +1,18 @@
-import bpy
+import sys
 
-
-def clear_meshes():
+def ConvertGLBtoFBX(input_filepath):
+    import bpy
+    output_filepath= input_filepath.replace(".glb",".fbx")
     bpy.ops.object.select_all(action='DESELECT')
     bpy.ops.object.select_by_type(type='MESH')
     bpy.ops.object.delete()
-
-def convert_glb_to_fbx(input_filepath, output_filepath):
-    clear_meshes()
     bpy.ops.import_scene.gltf(filepath=input_filepath)
     bpy.ops.export_scene.fbx(filepath=output_filepath)
+    return output_filepath
 
+def main():
+    input_filepath = sys.argv[-1]
+    ConvertGLBtoFBX(input_filepath)
 
-
-convert_glb_to_fbx("C:/Users/saleh/OneDrive/Documents/Work/Cinema 4D Plugin/file.glb", "C:/Users/saleh/OneDrive/Documents/Work/Cinema 4D Plugin/file.fbx")
-# "C:/Users/saleh/OneDrive/Documents/Work/Cinema 4D Plugin/file.glb"
-# "C:/Users/saleh/OneDrive/Documents/Work/Cinema 4D Plugin/file.fbx"
-# "C:/Users/saleh/OneDrive/Documents/Work/Cinema 4D Plugin/FBX_Convertor.py"
-# "C:/Program Files/Blender Foundation/Blender 4.0/blender.exe"
-# cmd.exe /C '"C:/Program Files/Blender Foundation/Blender 4.0/blender.exe" --background --python "C:/Users/saleh/OneDrive/Documents/Work/Cinema 4D Plugin/FBX_convertor.py"'
+if __name__ == "__main__":
+    main()
